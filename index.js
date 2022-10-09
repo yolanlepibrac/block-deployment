ab66de37a1710ab87c4082c2653440b3ecc0b29a
Yolan Pibrac <yolan.pibrac@gmail.com>
2022-10-09 10:37:43 +0200

    defined output depending on the PR names

--- index.js
+++ index.js
@@ -3,28 +3,25 @@ const github = require('@actions/github');
 
 async function main(){
     try {
-        // `credential` ans `pullRequestWords` inputs defined in action metadata file
         const token = core.getInput('github-token');
         const prName = core.getInput('pr-name');
         console.log(`Check if a PR exists with the word ${prName}`);
       
-        const buildNeedToBeCancelled = "true";
-        core.setOutput("cancel_build", buildNeedToBeCancelled);
-        console.log(`Build need to be cancelled ${buildNeedToBeCancelled}`);
-      
-      
         // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
+        // const payload = JSON.stringify(github.context.payload)
       
         const octokit = github.getOctokit(token)
-        const result = await octokit.rest.pulls.list({
+        const listPrResult = await octokit.rest.pulls.list({
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
           state: "open"
         })
+
+        const buildNeedToBeCancelled = listPrResult.data.find((pr) => pr.title.includes(prName))
+        const output = buildNeedToBeCancelled ? "true" : "false"
       
-        console.log(result)
+        core.setOutput("cancel_build", output);
+        console.log(`Build need to be cancelled ${output}`);
       
       } catch (error) {
         core.setFailed(error.message);
